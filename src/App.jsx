3bc739861ba2fd0e42c0fe4677d7d4e9bb19842b
Vijayalaxmi Shirnal <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2024-06-26 00:07:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <address id='address'>
+        A.G. PATIL INSTITUTE OF TECHNOLOGY SOLAPUR <br /> 
+        JV7R+FG8, Vijapur Rd, Opp. SRP Camp, 
+        Soregaon, Solapur, Maharashtra 413008ss
+      </address>
+      <br />
+      <div className="responsive-map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15211.018957716695!2d75.8919626!3d17.6143776!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc5d75b6d64eb25%3A0x31fb36694b3b0035!2sA.G.%20PATIL%20INSTITUTE%20OF%20TECHNOLOGY%20SOLAPUR!5e0!3m2!1sen!2sin!4v1719337813494!5m2!1sen!2sin" 
+      width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
